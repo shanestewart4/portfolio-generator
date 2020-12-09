1592399b73646829e5a32aba73a6aec84b364021
shanestewart4 <shanestewartguitar@gmail.com>
2020-12-09 09:20:29 -0600

    added inquirer'

--- app.js
+++ app.js
@@ -1,30 +1,25 @@
-const fs = require('fs');
+const inquirer = require('inquirer')
 
-const profileDataArgs = process.argv.slice(2);
+// const fs = require('fs');
 
-const [nombre, github] = profileDataArgs;
-
-const generatePage = (nombre, github) => {
-    return `
-    <!DOCTYPE html> 
-    <html lang="en"> 
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Portfolio Demo</title>
-    </head>
-    <body>
-      <h1>${nombre}</h1>
-      <h2><a href="https://github.com/${github}">Github</a></h2>
-    </body>
-    </html>
-    `;
-};
+// const generatePage = require('./src/page-template.js');
 
 
-fs.writeFile('./index.html', generatePage(nombre, github), err => {
-    if(err) throw new Error(err);
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-})
+// const pageHTML = generatePage(nombre, github)
+
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
+
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// })
